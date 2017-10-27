16607fb38b8a32016464cc29e83f984eabbd0a34
Jeancmarte21 <jeancmarte21@gmail.com>
2017-10-27 09:59:26 -0400

    sigo resolviendo tus dudas existenciales

--- appVS/resources/views/empleados/index.blade.php
+++ appVS/resources/views/empleados/index.blade.php
@@ -1,23 +1,8 @@
 
 @extends('layouts.admin')
 
-@section('contetenido')
+@section('contenido')
 
-<div class="panel panel-default">
-  <div class="panel-heading">Panel heading without title</div>
-  <div class="panel-body">
-
-
-  <ul class="list-group">
-  <li class="list-group-item">Cras justo odio</li>
-  <li class="list-group-item">Dapibus ac facilisis in</li>
-  <li class="list-group-item">Morbi leo risus</li>
-  <li class="list-group-item">Porta ac consectetur ac</li>
-  <li class="l
-ist-group-item">Vestibulum at eros</li>
-</ul>
-
-  </div>
-</div>
+ <h1>No se cuando aportaras algo, pero mientras seguire resolviendo. att: Jean tu papa Marte</h1>
 
 @endsection
