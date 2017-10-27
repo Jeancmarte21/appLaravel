5732ef89fb0bcc493533514fe10843ced4547002
Stanley De Moya <standmv13@gmail.com>
2017-10-27 01:24:21 -0400

    cambios

--- appVS/resources/views/empleados/index.blade.php
+++ appVS/resources/views/empleados/index.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
 <div class="panel panel-default">
   <div class="panel-heading">Panel heading without title</div>
   <div class="panel-body">
@@ -14,3 +17,5 @@ ist-group-item">Vestibulum at eros</li>
 
   </div>
 </div>
+
+@endsection
