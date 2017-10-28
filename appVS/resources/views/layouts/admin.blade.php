09bd52235c1801236bc38a5a46deab416f099d52
Jeancmarte21 <jeancmarte21@gmail.com>
2017-10-27 20:50:12 -0400

    likeo empleados/create

--- appVS/resources/views/layouts/admin.blade.php
+++ appVS/resources/views/layouts/admin.blade.php
@@ -104,7 +104,7 @@
                  <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-               <li><a href=""><i class="fa fa-circle-o"></i> Registrar</a></li>
+               <li><a href="empleados/create"><i class="fa fa-circle-o"></i> Registrar</a></li>
                 <li><a href="empleados"><i class="fa fa-circle-o"></i> Empleados</a></li>
                 
               </ul>
