6115c3a32106fd7542566c2196c9aeacbbfcc410
Jeancmarte21 <jeancmarte21@gmail.com>
2017-11-02 16:35:54 -0400

    tabla show

--- appVS/resources/views/empleados/show.blade.php
+++ appVS/resources/views/empleados/show.blade.php
@@ -2,11 +2,45 @@
 @section('title','Informacion Empleado')
 @section('contenido')
 
-<div class="card border-primary mb-3" style="max-width: 95rem;">
-  <div class="card-body text-primary">
-    <h4 class="card-title">{{$empleado->nombre.' '.$empleado->apellidos}}</h4>
-    <p class="card-text">{{$empleado->direccion}}</p>
-  </div>
+
+<div class="row">
+	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+		
+		
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div class="table-responsive">
+			<table class="table table-striped table-bordered table-condensed table-hover">
+				<thead>
+					<th>Id</th>
+					<th>Cedula</th>
+					<th>Nombre</th>
+					<th>Apellidos</th>
+					<th>Telefono</th>
+					<th>Direccion</th>	
+				</thead>
+
+			
+				<tr>
+					<td>{{$empleado->idempleado}}</td>
+					<th>{{$empleado->cedula}}</th>
+					<td>{{$empleado->nombre}}</td>
+					<td>{{$empleado->apellidos}}</td>
+					<th>{{$empleado->telefono}}</th>
+					<th>{{$empleado->direccion}}</th>
+					
+				
+				</tr>
+		        
+			
+              
+			</table>
+		</div>
+		
+	</div>
 </div>
 
 
