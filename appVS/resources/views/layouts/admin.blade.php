66ae783bc20213d4a58670c203acbff7fea4d32e
Jeancmarte21 <jeancmarte21@gmail.com>
2017-11-03 15:41:48 -0400

    back

--- appVS/resources/views/layouts/admin.blade.php
+++ appVS/resources/views/layouts/admin.blade.php
@@ -177,9 +177,13 @@
                 <div class="box-header with-border">
                   <h3 class="box-title">  <center>@yield('title','Default')</center></h3>
                   <div class="box-tools pull-right">
+                    <button class="btn btn-box-tool"><a href="{{ url()->previous() }}">Back</a><i class=""></i></button>
+
                     <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                     
                     <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+
+                    
                   </div>
                 </div>
                 <!-- /.box-header -->
