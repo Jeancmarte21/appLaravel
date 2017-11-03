93a3a1d81ff6fa7f3ac65838f6e7b7a3f2973c4b
Jeancmarte21 <jeancmarte21@gmail.com>
2017-11-03 15:48:29 -0400

    atras

--- appVS/resources/views/layouts/admin.blade.php
+++ appVS/resources/views/layouts/admin.blade.php
@@ -177,11 +177,11 @@
                 <div class="box-header with-border">
                   <h3 class="box-title">  <center>@yield('title','Default')</center></h3>
                   <div class="box-tools pull-right">
-                    <button class="btn btn-box-tool"><a href="{{ url()->previous() }}">Back</a><i class=""></i></button>
+                    <button class="btn btn-box-tool"><a href="{{ url()->previous() }}">Atras</a><i class=""></i></button>
 
-                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                   <!-- <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                     
-                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>-->
 
                     
                   </div>
