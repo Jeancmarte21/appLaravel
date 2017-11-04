12142e150339acd4a691a5eb9eca23c9bf9053ca
Jeancmarte21 <jeancmarte21@gmail.com>
2017-11-04 14:02:00 -0400

    likeo a inicio desde el nombre VS

--- appVS/resources/views/layouts/admin.blade.php
+++ appVS/resources/views/layouts/admin.blade.php
@@ -29,7 +29,7 @@
       <header class="main-header">
 
         <!-- Logo -->
-        <a href="index2.html" class="logo">
+        <a href="/inicios" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b>V</b>S</span>
           <!-- logo for regular state and mobile devices -->
