85bfdeb182d527cb82e837234f065d093af0a615
Jeancmarte21 <jeancmarte21@gmail.com>
2017-11-04 15:55:56 -0400

    cambio minimo (pero subi porque hare una pausa)

--- appVS/resources/views/jornadas/index.blade.php
+++ appVS/resources/views/jornadas/index.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.admin')
-@section('title','Jornada')
+@section('title','Listado de Jornadas')
 @section('contenido')
 
 <div class="row">
 	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-		<h3>Listado de Jornadas <a href="jornadas/create"><button class="btn btn-success btn btn-sm">Crear</button></a></h3>
 		@include('jornadas.search')
+		<h3><a href="jornadas/create"><button class="btn btn-success btn btn-md">CREAR NUEVA JORNADA</button></a></h3>
+		
 	</div>
 </div>
+
+
 
 <div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
