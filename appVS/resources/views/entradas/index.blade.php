4edba8187db376aae51b9d55dfefcd9e7e93858a
Jeancmarte21 <jeancmarte21@gmail.com>
2017-11-06 21:33:35 -0400

    bug fix

--- appVS/resources/views/entradas/index.blade.php
+++ appVS/resources/views/entradas/index.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
 	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
 		@include('entradas.search')
-		<h3><a href="entradas/create"><button class="btn btn-success btn btn-md">CREAR NUEVA ENTRADA</button></a></h3>
+		<h3><a href="entradas/create"><button class="btn btn-primary btn btn-md">CREAR NUEVA ENTRADA</button></a></h3>
 		
 	</div>
 </div>
