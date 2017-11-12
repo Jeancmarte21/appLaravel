c27dbbb9bc187089201bdeb73ef11e7843355c23
Stanley De Moya <standmv13@gmail.com>
2017-11-12 11:27:54 -0400

    maquinas

--- appVS/resources/views/maquinas/index.blade.php
+++ appVS/resources/views/maquinas/index.blade.php
@@ -2,42 +2,47 @@
 @section('title','Maquinas')
 @section('contenido')
 
+<div class="row">
+  <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+    <h3><a href="/maquinas/create"><button class="btn btn-primary btn btn-md">CREAR NUEVA MAQUINA</button></a></h3>
 
-<div class="list-group ">
+  </div>
+</div>
 
-	@foreach($maquinas as $maquina)
-  		<li class="list-group-item">{{$maquina->nombre}}
+<div class="row">
+  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="table-responsive">
+      <table class="table table-striped table-bordered table-condensed table-hover">
+        <thead>
+          <th>ID</th>
+          <th>NOMBRE</th>
+          <th>PAGA POR</th>
 
-			<a   
-              href="#"
-              class="badge badge-danger btn btn-danger"
-                  onclick="
-                  var result = confirm('Are you sure you wish to delete this Company?');
-                      if( result ){
-                              event.preventDefault();
-                              document.getElementById('delete-form').submit();
-                      }
-                          "
-                          >
-                  Eliminar
-              </a>
-
-             
-
-  		<a href="/maquinas/{{$maquina->idmaquina}}/edit" class="badge badge-success btn btn-info">Editar</a>
-
-  		<a href="/maquinas/{{$maquina->idmaquina}}" class="badge badge-primary btn btn-primary">Ver</a>
-
+          <th>OPCIONES</th>
+        </thead>
+        @foreach ($maquinas as $maquina)
+        <tr>
+          <td>{{$maquina->idmaquina}}</td>
+          <td>{{$maquina->nombre}}</td>
+          @if($maquina->produccion == 1) 
+          <td>Produccion</td>
+          @endif
+          @if($maquina->produccion == 0)
+          <td>Salario Fijo</td>
+          @endif
 
 
-  		</li>
-  	
-  	@endforeach
-  	</div>
-  	 <form id="delete-form" action="{{ route('maquinas.destroy',[$maquina->idmaquina]) }}" 
-                method="POST" style="display: none;">
-                        <input type="hidden" name="_method" value="delete">
-                        {{ csrf_field() }}
-              </form>
-  	
+          <td>
+            <a   
+                      href="/maquinas/{{$maquina->idmaquina}}"
+                      class="badge badge-info btn btn-success">Ver</a>
+                         <a href="/maquinas/{{$maquina->idmaquina}}/edit" class="badge badge-success btn btn-info">Editar</a>
+          </td>
+        </tr>
+
+        @endforeach
+      </table>
+    </div>
+  </div>
+</div> 	
 @endsection
