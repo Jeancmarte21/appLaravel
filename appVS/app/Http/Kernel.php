caa0ef096d637ba1bd7cd7a2d0348ede1a6794a7
Jeancmarte21 <jeancmarte21@gmail.com>
2017-11-14 15:48:44 -0400

    login entrando

--- appVS/app/Http/Kernel.php
+++ appVS/app/Http/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends HttpKernel
             \appVS\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+             \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \appVS\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
