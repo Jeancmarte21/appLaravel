4f54e9b10f4d94b40a19dfb96add85b86c983cf8
Jeancmarte21 <jeancmarte21@gmail.com>
2017-11-15 01:37:41 -0400

    cambio minimo

--- appVS/resources/views/layouts/admin.blade.php
+++ appVS/resources/views/layouts/admin.blade.php
@@ -246,9 +246,9 @@
                 <div class="box-header with-border">
                   <h3 class="box-title">  <center>@yield('title','Default')</center></h3>
                   <div class="box-tools pull-right">
-                    <button class="btn btn-box-tool"><a href="{{ url()->previous() }}">Atras</a><i class=""></i></button>
+                    <!-- <button class="btn btn-box-tool"><a href="{{ url()->previous() }}">Atras</a><i class=""></i></button>
 
-                   <!-- <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                   <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
 
                     <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>-->
 
