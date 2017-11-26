0cfc8eb294699082bcb90bc3f29820332fa75f8d
Jeancmarte21 <jeancmarte21@gmail.com>
2017-11-26 13:19:12 -0400

    cigarros con error

--- appVS/resources/views/cigarros/create.blade.php
+++ appVS/resources/views/cigarros/create.blade.php
@@ -1,14 +1,32 @@
-@extends('layouts.admin')
-@section('title','Crear Cigarro')
-@section('contenido')
+<div class="right_col" role="main">
+  <!--<div class="">-->
+    <div class="page-title">
 
-<!-- este es el form -->
+      <div class="clearfix"></div>
 
     <div class="row">
-  <div class="col-md-12">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+
     <form class="form-horizontal" method="post" action="/cigarros">
-      <fieldset>
+
       {{ csrf_field()}}
+
+      <h3>   <button  class="btn btn-primary btn btn-xs" data-toggle="modal" data-target=".bs-example-modal-lg"><i class="fa fa-user-plus" aria-hidden="true"></i> Crear Suplidor</button></h3>
+
+          <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+              <div class="modal-content">
+
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+                  </button>
+                  <h4 class="modal-title" id="myModalLabel">Crear Suplidor</h4>
+                </div>
+                <div class="modal-body">
+                    <form class="form-horizontal form-label-left input_mask">
+<div class="form-group">
 
                    	    <div class="form-group">
         <label class="col-xs-3 control-label">Nombre</label>
@@ -43,16 +61,26 @@
         </div>
     </div>
 
-                    </fieldset>
-                     <div class="form-group">
-        <div class="col-md-12 text-center">
-          <button type="submit" class="btn btn-primary btn-lg ">Registrar</button>
-        </div>
-      </div>
-      </fieldset>
-    </form>
-  </div>
+    <div class="modal-footer">
+         <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+
+         <button type="submit" class="btn btn-primary">Registrar</button>
+         <div class="form-group">
+
+       </div>
+     </form>
+</div>
+</div>
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
 </div>
 
 
-      @endsection
+
+</div>
+</div>
+</div>
